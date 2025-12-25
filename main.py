aae7902c0d1050b5ba8fd3a8492f39c36587da2f
Pavel Maslov <dnfanl@gmail.com>
2025-12-25 22:50:05 +0100

    book stats

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
-from stats import get_num_words, get_num_characters
+from stats import get_num_characters, get_num_words, sort_characters
+import sys
+
 
 def get_book_text(path_to_file):
-        with open (path_to_file) as f:
-            file_content = f.read()
-            return file_content
+    with open(path_to_file) as f:
+        file_content = f.read()
+        return file_content
+
 
 def main():
-    text = get_book_text("books/frankenstein.txt")
-    print("Found "+str(get_num_words(text)) +" total words")
-    print(get_num_characters(text))
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    path = sys.argv[1]
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at " + path + "...")
+    print("----------- Word Count ----------")
+    text = get_book_text(path)
+    print("Found " + str(get_num_words(text)) + " total words")
+    print("--------- Character Count -------")
+    characters = get_num_characters(text)
+    sorted_characters = sort_characters(characters)
+    print_char_dictionary(sorted_characters)
+    print("============= END ===============")
+
+
+def print_char_dictionary(characters):
+    for item in characters:
+        if item["char"].isalpha():
+            print(item["char"] + ": " + str(item["num"]))
+
 
 main()
--- stats.py
+++ stats.py
@@ -1,11 +1,23 @@
 def get_num_words(text):
-        return len(text.split())                                
+    return len(text.split())
+
 
 def get_num_characters(text):
     symbols = {}
     for symbol in text:
         normalized_symbol = symbol.lower()
-        count = symbols.get(normalized_symbol,0)
-        symbols.update({normalized_symbol:count+1})
+        count = symbols.get(normalized_symbol, 0)
+        symbols.update({normalized_symbol: count + 1})
     return symbols
 
+
+def sort_on(items):
+    return items["num"]
+
+
+def sort_characters(characters):
+    return sorted(
+        [{"char": k, "num": v} for k, v in characters.items()],
+        key=sort_on,
+        reverse=True,
+    )
