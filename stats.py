b7ed65106d4d66d4cef1a9e891cce8b206f890e3
Pavel Maslov <dnfanl@gmail.com>
2025-12-26 12:57:35 +0100

    Counter added

--- stats.py
+++ stats.py
@@ -1,13 +1,12 @@
+from collections import Counter
+
+
 def get_num_words(text):
     return len(text.split())
 
 
 def get_num_characters(text):
-    symbols = {}
-    for symbol in text:
-        normalized_symbol = symbol.lower()
-        count = symbols.get(normalized_symbol, 0)
-        symbols.update({normalized_symbol: count + 1})
+    symbols = Counter(text)
     return symbols
 
 
